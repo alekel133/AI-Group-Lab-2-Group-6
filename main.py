42b59c934bc73851d746575256261312b9eebd05
Alexander Kellough <atk133@msstate.edu>
2024-03-18 21:36:09 -0500

    Uploaded Wrong main.py

--- main.py
+++ main.py
@@ -1,111 +1,104 @@
-from PIL import Image, ImageFilter
-import random as rand
-import string
-import numpy as np
+# Welcome to
+# __________         __    __  .__                               __
+# \______   \_____ _/  |__/  |_|  |   ____   ______ ____ _____  |  | __ ____
+#  |    |  _/\__  \\   __\   __\  | _/ __ \ /  ___//    \\__  \ |  |/ // __ \
+#  |    |   \ / __ \|  |  |  | |  |_\  ___/ \___ \|   |  \/ __ \|    <\  ___/
+#  |________/(______/__|  |__| |____/\_____>______>___|__(______/__|__\\_____>
+#
+# This file can be a nice home for your Battlesnake logic and helper functions.
+#
+# To get you started we've included code to prevent your Battlesnake from moving backwards.
+# For more info see docs.battlesnake.com
+
+import random
+import typing
 import sys
 
-chars = {
-        "0": "../font/0.png","1": "../font/1.png","2": "../font/2.png",
-        "3": "../font/3.png","4": "../font/4.png","5": "../font/5.png",
-        "6": "../font/6.png","7": "../font/7.png","8": "../font/8.png",
-        "9": "../font/9.png","A": "../font/A.png","B": "../font/B.png",
-        "C": "../font/C.png","D": "../font/D.png","E": "../font/E.png",
-        "F": "../font/F.png","G": "../font/G.png","H": "../font/H.png",
-        "I": "../font/I.png","J": "../font/J.png","K": "../font/K.png",
-        "L": "../font/L.png","M": "../font/M.png","N": "../font/N.png",
-        "O": "../font/O.png","P": "../font/P.png","Q": "../font/Q.png",
-        "R": "../font/R.png","S": "../font/S.png","T": "../font/T.png",
-        "U": "../font/U.png","V": "../font/V.png","W": "../font/W.png",
-        "X": "../font/X.png","Y": "../font/Y.png","Z": "../font/Z.png",
-        "?": "../font/quest.png",".": "../font/dot.png","!": "../font/bang.png",
-}
 
-def resize(sizeFactor, text, Image, a, b, height, j):
-    print(sizeFactor)
-    a_nearest = int(np.round(a*sizeFactor))
-    b_nearest = int(np.round(b*sizeFactor))
-    height = int(np.round(height*sizeFactor))
-    for h in range(a_nearest):
-        for v in range(b_nearest):
-            Image.putpixel((h, v), Image.getpixel((h,v)))
-    a = a_nearest
-    b = b_nearest
-    return Image, a, b, height
+# info is called when you create your Battlesnake on play.battlesnake.com
+# and controls your Battlesnake's appearance
+# TIP: If you open your Battlesnake URL in a browser you should see this data
+def info() -> typing.Dict:
+    print("INFO")
+
+    return {
+        "apiversion": "1",
+        "author": "alekel133",  # TODO: Your Battlesnake Username
+        "color": "#FFC0CB",  # TODO: Choose color
+        "head": "default",  # TODO: Choose head
+        "tail": "default",  # TODO: Choose tail
+    }
 
 
-
-def main():
-    # Load command line arguments
-    text = None
-    oName = None
-    sizeFactor = 0.9
-    size = False
-    for x in range(len(sys.argv)):
-        if sys.argv[x] == "-i":
-            text = sys.argv[x+1]
-            x = x+2
-        elif sys.argv[x] == "-o":
-            oName = sys.argv[x+1]
-            x = x+2
-        elif sys.argv[x] == "-s":
-            sizeFactor = float(sys.argv[x+1])
-            size = True
-            x += 2
+# start is called when your Battlesnake begins a game
+def start(game_state: typing.Dict):
+    print("GAME START")
 
 
-    if text == None:
-        raise Exception("No text was provide")
-
-    if oName == None:
-        oName =''.join(rand.choices(string.ascii_letters + string.digits, k = 20))
-
-# Copy Background Image
-    bgimg = Image.open("../test-images/canvas.png")
-    outImage = bgimg.copy()
+# end is called when your Battlesnake finishes a game
+def end(game_state: typing.Dict):
+    print("GAME OVER\n")
 
 
-    i,j = outImage.size
-    x = 0
-    y = 0
-    height = 38
-    # Put text on Copied Background
-    for char in text:
-        if(char == " "):
-            x += 12
-            continue
-        if(char.isalpha()):
-            char = char.upper() 
-        if char is None:
-            raise Exception("Error! Invalid Character!")
+# move is called on every turn and returns your next move
+# Valid moves are "up", "down", "left", or "right"
+# See https://docs.battlesnake.com/api/example-move for available data
+def move(game_state: typing.Dict) -> typing.Dict:
 
-        tmpImg = Image.open(chars.get(char))
-        tmpImg = tmpImg.convert("RGBA")
-        a,b = tmpImg.size
+    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
 
-        
-        if size == True:
-           tmpImg, a, b, height = resize(sizeFactor, text, tmpImg, a, b, height, j)
+    # We've included code to prevent your Battlesnake from moving backwards
+    my_head = game_state["you"]["body"][0]  # Coordinates of your head
+    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
 
-        if(a+x) > i:
-            y += 40
-            x = 0
+    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
+        is_move_safe["left"] = False
 
-        for h in range(a):
-            for v in range(b):
-                if tmpImg.getpixel((h,v)) == (0, 255, 0, 255):
-                    continue
-                else:
-                    outImage.putpixel((x+h, y+v), tmpImg.getpixel((h,v)))
+    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
+        is_move_safe["right"] = False
 
-                
-        x += a
-        tmpImg.close()
+    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
+        is_move_safe["down"] = False
 
-    outImage.show()
+    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
+        is_move_safe["up"] = False
 
-    oName = oName + ".png"
-    print(oName)
-    outImage.save(oName)
-    outImage.close()
+    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
+    # board_width = game_state['board']['width']
+    # board_height = game_state['board']['height']
 
-main()
+    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
+    # my_body = game_state['you']['body']
+
+    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
+    # opponents = game_state['board']['snakes']
+
+    # Are there any safe moves left?
+    safe_moves = []
+    for move, isSafe in is_move_safe.items():
+        if isSafe:
+            safe_moves.append(move)
+
+    if len(safe_moves) == 0:
+        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
+        return {"move": "down"}
+
+    # Choose a random move from the safe ones
+    next_move = random.choice(safe_moves)
+
+    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
+    # food = game_state['board']['food']
+
+    print(f"MOVE {game_state['turn']}: {next_move}")
+    return {"move": next_move}
+
+
+# Start server when `python main.py` is run
+if __name__ == "__main__":
+    from server import run_server
+    port = "8000"
+    for i in range(len(sys.argv) - 1):
+        if sys.argv[i] == '--port':
+            port = sys.argv[i+1]
+
+    run_server({"info": info, "start": start, "move": move, "end": end, "port": port})
